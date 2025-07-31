89247cea0abf1458e004429908fba49a931ad512
minkyong00 <minkyong131@gmail.com>
2025-07-31 16:39:08 +0900

    service test 중

--- src/main/CrimeServiceTest.java
+++ src/main/CrimeServiceTest.java
@@ -1,9 +1,14 @@
 package main;
 
 import java.sql.SQLException;
+import java.util.List;
 
+import model.Crime;
+import model.Region;
 import service.CrimeService;
+import service.RegionService;
 import service.Impl.CrimeServiceImpl;
+import service.Impl.RegionServiceImpl;
 import util.APIUtil.CrimeAPI;
 
 public class CrimeServiceTest {
@@ -11,9 +16,16 @@ public class CrimeServiceTest {
 	public static void main(String[] args) {
 	
 		CrimeService crimeService = new CrimeServiceImpl();
+		RegionService regionService = new RegionServiceImpl();
 		
 		try {
 			
+			List<Crime> crimeList = CrimeAPI.getCrimeList(CrimeAPI.getCrimeAPI());
+			for(Crime crime : crimeList) {
+				crimeService.insertCrime(
+						new Crime(0, crime.getCrimeYear(), 0, crime.getCrimeType(), crime.getCrimeCount(), crime.getRegion())
+				);
+			}
 			
 			
 		} catch (SQLException sqle) {
