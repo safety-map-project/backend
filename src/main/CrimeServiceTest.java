2820d82a69c505d4605d610e99d2f44c640ae5f3
minkyong00 <minkyong131@gmail.com>
2025-07-31 16:09:41 +0900

    범죄발생지 api list에 추가

--- src/main/CrimeServiceTest.java
+++ src/main/CrimeServiceTest.java
@@ -1,8 +1,25 @@
 package main;
 
+import java.sql.SQLException;
+
+import service.CrimeService;
+import service.Impl.CrimeServiceImpl;
+import util.APIUtil.CrimeAPI;
+
 public class CrimeServiceTest {
 
 	public static void main(String[] args) {
+	
+		CrimeService crimeService = new CrimeServiceImpl();
+		
+		try {
+			
+			
+			
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+	
 	}
 	
 }
