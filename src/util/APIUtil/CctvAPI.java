6e56f400ff8c65e9e6b2535a1f15aa0b1cb8afb9
safety-map-project <scotteee7@gmail.com>
2025-08-04 10:48:01 +0900

    update CctvAPI

--- src/util/APIUtil/CctvAPI.java
+++ src/util/APIUtil/CctvAPI.java
@@ -1,5 +1,40 @@
 package util.APIUtil;
 
-public class CctvAPI {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class CctvAPI {
+	JsonArray cctvs;
+	
+	public JsonArray cctvArray() {
+		File file = null;
+		Reader reader = null;
+		
+		
+		try {
+			file = new File("../file/CCTV.json");
+			reader = new FileReader(file);
+			
+			Gson gson = new Gson();
+			JsonObject obj = JsonParser.parseReader(reader).getAsJsonObject();
+			JsonArray cctvs = obj.getAsJsonArray();
+		}catch (IOException ioe){
+			ioe.printStackTrace();
+		}finally {
+			try {
+				reader.close();
+			}catch (Exception ex){
+				ex.printStackTrace();
+			}
+		}
+		
+		return cctvs;
+	}
 }
