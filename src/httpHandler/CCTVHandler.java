6c57ebc6b96d00a36722036a5e4cc3c3c082e2c0
jeonghyeonoo <jeonghyeonoo@naver.com>
2025-08-07 16:30:29 +0900

    update CCTVHandler

--- src/httpHandler/CCTVHandler.java
+++ src/httpHandler/CCTVHandler.java
@@ -1,28 +1,31 @@
 package httpHandler;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
-import com.google.gson.JsonArray;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import dao.CctvDao;
+import dao.Impl.CctvDaoImpl;
 import util.HandlerUtil;
-import util.APIUtil.CctvAPI;
 
 public class CCTVHandler implements HttpHandler {
-
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
 		
 		if(exchange.getRequestMethod().equals("GET")) {
+			CctvDao CCTVDao = new CctvDaoImpl();
 			String cctvStr = "";
 			
-			JsonArray cctvs = CctvAPI.getCctvArray();
-			cctvStr = cctvs.toString();
+			try {
+				cctvStr = CCTVDao.listCctv().toString();
+			}catch (SQLException sqle) {
+				sqle.printStackTrace();
+			}
 			
 			HandlerUtil.sendResponse(exchange, cctvStr);
-			
 		}
+		
 	};
-
 }
